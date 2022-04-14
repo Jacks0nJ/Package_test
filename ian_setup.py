c37188f54ed42b15b5a257a676eab6b0ca46039b
Jacks0nJ <joseph.jackson@port.ac.uk>
2022-04-14 10:19:15 +0100

    Hopefully fixed Cython

---
+++ ian_setup.py
@@ -0,0 +1,43 @@
+
+import os
+
+from setuptools import setup, Extension
+
+
+from Cython.Build import cythonize
+
+__author__ = "Joseph Jackson <joseph.jackson@port.ac.uk>"
+
+
+# define cython options
+cython_compile_args = [
+    "-O3"
+]
+
+cython_directives = {
+    "language_level": 3,
+}
+
+# enable coverage for cython
+if int(os.getenv("CYTHON_LINETRACE", "0")):
+    cython_directives["linetrace"] = True
+    cython_compile_args.append("-DCYTHON_TRACE")
+
+# define compiled extensions
+exts = [
+    Extension(
+        "src.example_package.fibonacci",
+        ["src/example_package/fibonacci.pyx"],
+        language="c",
+        libraries=["lal"],
+        extra_compile_args=cython_compile_args,
+        extra_link_args=[],
+    ),
+]
+
+# -- build the thing
+# this function only manually specifies things that aren't
+# supported by setup.cfg (as of setuptools-30.3.0)
+setup(
+    ext_modules=cythonize(exts, compiler_directives=cython_directives),
+)
